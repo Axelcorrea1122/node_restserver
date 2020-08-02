f99c3475f3c5b147d29ac04d231ffa4016cd7194
Axel <axelcorrea1122@gmail.com>
2020-08-02 19:04:13 -0300

    Otro commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,4 @@
 process.env.PORT = process.env.PORT || 3000 //puerto
+
+
+/* UN TIP PARA EL POSTMAN ES CREAR AMBIENTES UNO DE PRODUCCION Y OTRO DE DESARROLLO PARA PONER EN LA URL {{url}}/usuario y elegimos el ambiente donde queremos mandar la peticion */
